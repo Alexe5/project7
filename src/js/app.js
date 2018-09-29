c135ba68eda9641b6f14903949253906f45aee01
Al.exe <urbanmaugli@gmail.com>
2018-09-29 14:13:30 +0300

    hover

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 import sayHello from './lib/sayHello.js';
+import makeHover from './lib/makeHover.js';
 
 sayHello();
---
+++ src/js/lib/makeHover.js
@@ -0,0 +1,125 @@
+$('.row1 .table__text4').mouseover(function() {
+	$('.row1 .table__text1, .row1 .table__text2, .row1 .table__text3').toggleClass('table__active');
+});
+$('.row1 .table__text4').mouseout(function() {
+	$('.row1 .table__text1, .row1 .table__text2, .row1 .table__text3').toggleClass('table__active');
+});
+
+
+$('.row2 .table__text4').mouseover(function() {
+	$('.row2 .table__text1, .row2 .table__text2, .row2 .table__text3').toggleClass('table__active');
+});
+$('.row2 .table__text4').mouseout(function() {
+	$('.row2 .table__text1, .row2 .table__text2, .row2 .table__text3').toggleClass('table__active');
+});
+
+
+$('.row3 .table__text4').mouseover(function() {
+	$('.row3 .table__text1, .row3 .table__text2, .row3 .table__text3').toggleClass('table__active');
+});
+$('.row3 .table__text4').mouseout(function() {
+	$('.row3 .table__text1, .row3 .table__text2, .row3 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row4 .table__text4').mouseover(function() {
+	$('.row4 .table__text1, .row4 .table__text2, .row4 .table__text3').toggleClass('table__active');
+});
+$('.row4 .table__text4').mouseout(function() {
+	$('.row4 .table__text1, .row4 .table__text2, .row4 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row5 .table__text4').mouseover(function() {
+	$('.row5 .table__text1, .row5 .table__text2, .row5 .table__text3').toggleClass('table__active');
+});
+$('.row5 .table__text4').mouseout(function() {
+	$('.row5 .table__text1, .row5 .table__text2, .row5 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row6 .table__text4').mouseover(function() {
+	$('.row6 .table__text1, .row6 .table__text2, .row6 .table__text3').toggleClass('table__active');
+});
+$('.row6 .table__text4').mouseout(function() {
+	$('.row6 .table__text1, .row6 .table__text2, .row6 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row7 .table__text4').mouseover(function() {
+	$('.row7 .table__text1, .row7 .table__text2, .row7 .table__text3').toggleClass('table__active');
+});
+$('.row7 .table__text4').mouseout(function() {
+	$('.row7 .table__text1, .row7 .table__text2, .row7 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row8 .table__text4').mouseover(function() {
+	$('.row8 .table__text1, .row8 .table__text2, .row8 .table__text3').toggleClass('table__active');
+});
+$('.row8 .table__text4').mouseout(function() {
+	$('.row8 .table__text1, .row8 .table__text2, .row8 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row9 .table__text4').mouseover(function() {
+	$('.row9 .table__text1, .row9 .table__text2, .row9 .table__text3').toggleClass('table__active');
+});
+$('.row9 .table__text4').mouseout(function() {
+	$('.row9 .table__text1, .row9 .table__text2, .row9 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row10 .table__text4').mouseover(function() {
+	$('.row10 .table__text1, .row10 .table__text2, .row10 .table__text3').toggleClass('table__active');
+});
+$('.row10 .table__text4').mouseout(function() {
+	$('.row10 .table__text1, .row10 .table__text2, .row10 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row11 .table__text4').mouseover(function() {
+	$('.row11 .table__text1, .row11 .table__text2, .row11 .table__text3').toggleClass('table__active');
+});
+$('.row11 .table__text4').mouseout(function() {
+	$('.row11 .table__text1, .row11 .table__text2, .row11 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row12 .table__text4').mouseover(function() {
+	$('.row12 .table__text1, .row12 .table__text2, .row12 .table__text3').toggleClass('table__active');
+});
+$('.row12 .table__text4').mouseout(function() {
+	$('.row12 .table__text1, .row12 .table__text2, .row12 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row13 .table__text4').mouseover(function() {
+	$('.row13 .table__text1, .row13 .table__text2, .row13 .table__text3').toggleClass('table__active');
+});
+$('.row13 .table__text4').mouseout(function() {
+	$('.row13 .table__text1, .row13 .table__text2, .row13 .table__text3').toggleClass('table__active');
+});
+
+
+
+$('.row14 .table__text4').mouseover(function() {
+	$('.row14 .table__text1, .row14 .table__text2, .row14 .table__text3').toggleClass('table__active');
+});
+$('.row14 .table__text4').mouseout(function() {
+	$('.row14 .table__text1, .row14 .table__text2, .row14 .table__text3').toggleClass('table__active');
+});
+
+
+
+module.exports = makeHover;
