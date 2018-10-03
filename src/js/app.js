0c0b58b48dc1fe3282ecadb8b9a6ad720af8b207
Al.exe <urbanmaugli@gmail.com>
2018-10-03 05:19:54 +0300

    js optimization

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,1 @@
-import sayHello from './lib/sayHello.js';
-import makeHover from './lib/makeHover.js';
 import openPopup from './lib/openPopup.js';
-
-sayHello();
--- src/js/lib/openPopup.js
+++ src/js/lib/openPopup.js
@@ -1,3 +1,7 @@
+// ----------------------------------
+// ---------------Menu---------------
+// ----------------------------------
+
 $('.toggle').click(function() {
 	$('.menu').css('display', 'flex');
 });
@@ -6,87 +10,15 @@ $('.menu__close').click(function() {
 	$('.menu').css('display', 'none');
 });
 
-$('.row1 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row1 .table__text2').text();
-	$('.popup__text').append(message);
-});
+// ------------------------------------
+// ---------------Pop-up---------------
+// ------------------------------------
 
-$('.row2 .table__text4').click(function() {
+$('.table__text4').click(function() {
+	var number = $(this).data('id');
+	var selector = '.row' + number + ' .table__text2';
+	var message = $(selector).text();
 	$('.popup').fadeIn(500);
-	var message = $('.row2 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row3 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row3 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row4 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row4 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row5 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row5 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row6 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row6 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row7 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row7 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row8 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row8 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row9 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row9 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row10 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row10 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row11 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row11 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row12 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row12 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row13 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row13 .table__text2').text();
-	$('.popup__text').append(message);
-});
-
-$('.row14 .table__text4').click(function() {
-	$('.popup').fadeIn(500);
-	var message = $('.row14 .table__text2').text();
 	$('.popup__text').append(message);
 });
 
@@ -94,5 +26,89 @@ $('.popup__close').click(function() {
 	$('.popup').fadeOut(0);
 	$('.popup__text').text('');
 });
+
+// $('.row1 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row1 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row2 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row2 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row3 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row3 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row4 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row4 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row5 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row5 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row6 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row6 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row7 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row7 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row8 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row8 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row9 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row9 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row10 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row10 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row11 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row11 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row12 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row12 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row13 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row13 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
+
+// $('.row14 .table__text4').click(function() {
+// 	$('.popup').fadeIn(500);
+// 	var message = $('.row14 .table__text2').text();
+// 	$('.popup__text').append(message);
+// });
 
 module.exports = openPopup;
